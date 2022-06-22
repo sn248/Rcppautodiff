793ca38d0466fbfec0d4cf677e9028581e4b66f1
quantilogy <satyaprakash.nayak@hotmail.com>
2022-06-22 16:30:31 -0400

    Added Rcppautodiff.h

---
+++ inst/include/Rcppautodiff.h
@@ -0,0 +1,26 @@
+// Rcppautodiff.h
+//
+// Copyright (C) 2022 Satyaprakash Nayakayak
+//
+// This file is part of Rcppautodiff
+//
+// Rcppautofiff is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Rcppautodiff is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with RcppDist.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef Rcppautodiff__Rcppautodiff__h
+#define Rcppautodiff__Rcppautodiff__h
+
+#include <Rcpp.h>
+
+#endif
+
